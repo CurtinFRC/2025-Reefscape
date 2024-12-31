148dcda035eeba2a018a0613e528a1fccbefaed8
Jade Turner <spacey-sooty@proton.me>
2025-01-01 01:31:12 +0800

    Choreo
    Signed-off-by: Jade Turner <spacey-sooty@proton.me>

---
+++ README.md
@@ -0,0 +1,8 @@
+# 2025 Reefscape
+
+## Simulation GUI
+Run with -PsimGui for the simulation gui. This shouldn't be used when doing replay.
+
+## TODO:
+- ctre force feedforward with Choreo
+- better gyro sim
--- src/main/java/org/curtinfrc/BuildConstants.java
+++ src/main/java/org/curtinfrc/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025-Reefscape";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 7;
-  public static final String GIT_SHA = "39b038aa64ee1c988fdc1dc405440a69e8ca59bc";
-  public static final String GIT_DATE = "2024-12-20 09:04:08 EST";
+  public static final int GIT_REVISION = 12;
+  public static final String GIT_SHA = "8113d83081a19ca55eb4dc68c4a20c58f8496dc9";
+  public static final String GIT_DATE = "2025-01-01 12:25:00 EST";
   public static final String GIT_BRANCH = "master";
-  public static final String BUILD_DATE = "2024-12-20 09:44:54 EST";
-  public static final long BUILD_UNIX_TIME = 1734705894406L;
+  public static final String BUILD_DATE = "2025-01-01 12:30:35 EST";
+  public static final long BUILD_UNIX_TIME = 1735752635965L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
