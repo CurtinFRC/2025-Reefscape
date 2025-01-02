fdabad9383eba4e8b053c05fb9fdf59e58d2db51
Jade <spacey-sooty@proton.me>
2025-01-02 22:26:05 +0800

    Miscellaneous vision patches (#3)
    - PhotonVision single tag support
    - Fix sim using camera0 offset for both cameras
    
    Signed-off-by: Jade Turner <spacey-sooty@proton.me>

--- src/main/java/org/curtinfrc/frc2025/BuildConstants.java
+++ src/main/java/org/curtinfrc/frc2025/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025-Reefscape";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 16;
-  public static final String GIT_SHA = "ba7d57414ebef7bf5738ae93e0a8b095d25c02f7";
-  public static final String GIT_DATE = "2025-01-02 22:12:45 AWST";
-  public static final String GIT_BRANCH = "jade/rename-package";
-  public static final String BUILD_DATE = "2025-01-02 22:14:34 AWST";
-  public static final long BUILD_UNIX_TIME = 1735827274896L;
+  public static final int GIT_REVISION = 17;
+  public static final String GIT_SHA = "4f039f7d1efef380375ffb630ad482fe36cc434c";
+  public static final String GIT_DATE = "2025-01-02 22:20:39 AWST";
+  public static final String GIT_BRANCH = "jade/vision-patchees";
+  public static final String BUILD_DATE = "2025-01-02 22:23:28 AWST";
+  public static final long BUILD_UNIX_TIME = 1735827808107L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
