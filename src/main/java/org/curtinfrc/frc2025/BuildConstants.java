5a657ed55966827cc643e6e6b1c9660a580b02f2
Jade Turner <spacey-sooty@proton.me>
2025-01-03 10:29:36 +0800

    [build] Update to 2025 stable dependencies
    PhotonVision and Choreo stable aren't out yet so we'll use thir
    published versions compatible with stable WPILib.
    - Update to stable REVLib
    - Update to stable Phoenix6
    - Update to stable URCL
    - Update to stable AdvantageKit
    
    Signed-off-by: Jade Turner <spacey-sooty@proton.me>

--- src/main/java/org/curtinfrc/frc2025/BuildConstants.java
+++ src/main/java/org/curtinfrc/frc2025/BuildConstants.java
@@ -5,13 +5,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025-Reefscape";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 28;
-  public static final String GIT_SHA = "691217e47a4817d73d32992bf9b85386f7fe097a";
-  public static final String GIT_DATE = "2025-01-13 15:51:47 AWST";
-  public static final String GIT_BRANCH = "paul/elevator-subsystem";
-  public static final String BUILD_DATE = "2025-01-13 15:51:57 AWST";
-  public static final long BUILD_UNIX_TIME = 1736754717573L;
-  public static final int DIRTY = 0;
+  public static final int GIT_REVISION = 26;
+  public static final String GIT_SHA = "8094eede2fe3f623a77141baf8fec2f94d7f662b";
+  public static final String GIT_DATE = "2025-01-16 18:03:56 AWST";
+  public static final String GIT_BRANCH = "jade/2025-kickoff";
+  public static final String BUILD_DATE = "2025-01-16 18:12:25 AWST";
+  public static final long BUILD_UNIX_TIME = 1737022345060L;
+  public static final int DIRTY = 1;
 
   private BuildConstants() {}
 }
