f55eefa824188fc1b74616781f6e2fe4802b9f87
Jade Turner <spacey-sooty@proton.me>
2025-01-06 23:31:08 +0800

    Basic autoalign
    Signed-off-by: Jade Turner <spacey-sooty@proton.me>

--- src/main/java/org/curtinfrc/frc2025/BuildConstants.java
+++ src/main/java/org/curtinfrc/frc2025/BuildConstants.java
@@ -5,13 +5,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025-Reefscape";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 28;
-  public static final String GIT_SHA = "691217e47a4817d73d32992bf9b85386f7fe097a";
-  public static final String GIT_DATE = "2025-01-13 15:51:47 AWST";
-  public static final String GIT_BRANCH = "paul/elevator-subsystem";
-  public static final String BUILD_DATE = "2025-01-13 15:51:57 AWST";
-  public static final long BUILD_UNIX_TIME = 1736754717573L;
-  public static final int DIRTY = 0;
+  public static final int GIT_REVISION = 21;
+  public static final String GIT_SHA = "7bd906c65a2681f51ad4678e44c3439b7587027c";
+  public static final String GIT_DATE = "2025-01-08 20:58:27 AWST";
+  public static final String GIT_BRANCH = "HEAD";
+  public static final String BUILD_DATE = "2025-01-08 21:04:38 AWST";
+  public static final long BUILD_UNIX_TIME = 1736341478937L;
+  public static final int DIRTY = 1;
 
   private BuildConstants() {}
 }
