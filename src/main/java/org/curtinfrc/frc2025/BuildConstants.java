631c76ea41d142d0606dec79a837e26bf13ff305
Jade Turner <spacey-sooty@proton.me>
2025-01-08 22:12:34 +0800

    ooooh fancy vector fields
    Signed-off-by: Jade Turner <spacey-sooty@proton.me>

--- src/main/java/org/curtinfrc/frc2025/BuildConstants.java
+++ src/main/java/org/curtinfrc/frc2025/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025-Reefscape";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 21;
-  public static final String GIT_SHA = "7bd906c65a2681f51ad4678e44c3439b7587027c";
-  public static final String GIT_DATE = "2025-01-08 20:58:27 AWST";
-  public static final String GIT_BRANCH = "HEAD";
-  public static final String BUILD_DATE = "2025-01-08 21:04:38 AWST";
-  public static final long BUILD_UNIX_TIME = 1736341478937L;
+  public static final int GIT_REVISION = 22;
+  public static final String GIT_SHA = "da5455019b46522eb0e0ee48ab47e9cae497ed4d";
+  public static final String GIT_DATE = "2025-01-08 21:04:45 AWST";
+  public static final String GIT_BRANCH = "jade/auto-align";
+  public static final String BUILD_DATE = "2025-01-08 22:10:25 AWST";
+  public static final long BUILD_UNIX_TIME = 1736345425702L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
