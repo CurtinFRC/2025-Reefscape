02ae6c7e7049c8c2fd7e58526b6783f71ff69906
Jade <spacey-sooty@proton.me>
2025-01-12 14:25:44 +0800

    Add Crayon CAD robot model (#13)
    Signed-off-by: Jade Turner <spacey-sooty@proton.me>

--- src/main/java/org/curtinfrc/frc2025/BuildConstants.java
+++ src/main/java/org/curtinfrc/frc2025/BuildConstants.java
@@ -5,13 +5,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025-Reefscape";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 20;
-  public static final String GIT_SHA = "86620c58a2b41eaf8e4f22258d51063e3033fd36";
-  public static final String GIT_DATE = "2025-01-08 20:49:09 AWST";
-  public static final String GIT_BRANCH = "HEAD";
-  public static final String BUILD_DATE = "2025-01-08 20:51:13 AWST";
-  public static final long BUILD_UNIX_TIME = 1736340673960L;
-  public static final int DIRTY = 1;
+  public static final int GIT_REVISION = 23;
+  public static final String GIT_SHA = "3c8b5d6acc588a58785950fc6ac96e6158a1861f";
+  public static final String GIT_DATE = "2025-01-12 11:12:35 AWST";
+  public static final String GIT_BRANCH = "jade/custom-robot";
+  public static final String BUILD_DATE = "2025-01-12 12:40:28 AWST";
+  public static final long BUILD_UNIX_TIME = 1736656828044L;
+  public static final int DIRTY = 0;
 
   private BuildConstants() {}
 }
