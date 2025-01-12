83926306f362e32acc1f755458247f3e03520041
Paul Hodges <paul.hodges@student.education.wa.edu.au>
2025-01-12 11:10:31 +0800

    new field plan

--- src/main/java/org/curtinfrc/frc2025/BuildConstants.java
+++ src/main/java/org/curtinfrc/frc2025/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025-Reefscape";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 22;
-  public static final String GIT_SHA = "da5455019b46522eb0e0ee48ab47e9cae497ed4d";
-  public static final String GIT_DATE = "2025-01-08 21:04:45 AWST";
+  public static final int GIT_REVISION = 23;
+  public static final String GIT_SHA = "5e3e09f66b44cd80fa8200ebd4adf5e175578b04";
+  public static final String GIT_DATE = "2025-01-08 22:12:34 AWST";
   public static final String GIT_BRANCH = "jade/auto-align";
-  public static final String BUILD_DATE = "2025-01-08 22:10:25 AWST";
-  public static final long BUILD_UNIX_TIME = 1736345425702L;
+  public static final String BUILD_DATE = "2025-01-12 11:06:10 AWST";
+  public static final long BUILD_UNIX_TIME = 1736651170523L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
