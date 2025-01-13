076fa7b376d90380e7e5ca561487560467419dd5
Paul Hodges <paul.hodges@student.education.wa.edu.au>
2025-01-13 16:16:19 +0800

    formatting

--- src/main/java/org/curtinfrc/frc2025/Robot.java
+++ src/main/java/org/curtinfrc/frc2025/Robot.java
@@ -157,7 +157,6 @@ public class Robot extends LoggedRobot {
                   new VisionIOLimelight(camera1Name, drive::getRotation),
                   new VisionIOQuestNav());
           elevator = new Elevator(new ElevatorIONeoMaxMotionLaserCAN());
-          
         }
 
         case SIMBOT -> {
