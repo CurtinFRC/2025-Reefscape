e4c60ee7ba87b20350b0b40746c60b8dfa9f269b
Paul Hodges <paul.hodges@student.education.wa.edu.au>
2025-01-13 15:52:27 +0800

    did elevator

--- src/main/java/org/curtinfrc/frc2025/BuildConstants.java
+++ src/main/java/org/curtinfrc/frc2025/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025-Reefscape";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 25;
-  public static final String GIT_SHA = "98157a30b622cb7f81067b0160ce38c45bb95dec";
-  public static final String GIT_DATE = "2025-01-09 19:30:53 AWST";
-  public static final String GIT_BRANCH = "paul/elevator";
-  public static final String BUILD_DATE = "2025-01-09 19:30:58 AWST";
-  public static final long BUILD_UNIX_TIME = 1736422258860L;
+  public static final int GIT_REVISION = 28;
+  public static final String GIT_SHA = "691217e47a4817d73d32992bf9b85386f7fe097a";
+  public static final String GIT_DATE = "2025-01-13 15:51:47 AWST";
+  public static final String GIT_BRANCH = "paul/elevator-subsystem";
+  public static final String BUILD_DATE = "2025-01-13 15:51:57 AWST";
+  public static final long BUILD_UNIX_TIME = 1736754717573L;
   public static final int DIRTY = 0;
 
   private BuildConstants() {}
