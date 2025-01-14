a7a891d651ef79bd9f018c75edc94cfb7475b247
Paul Hodges <paul.hodges@student.education.wa.edu.au>
2025-01-14 10:22:08 +0800

    minor fix

--- src/main/java/org/curtinfrc/frc2025/BuildConstants.java
+++ src/main/java/org/curtinfrc/frc2025/BuildConstants.java
@@ -5,13 +5,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025-Reefscape";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 38;
-  public static final String GIT_SHA = "e69de318e0a1db056bf5b17bbe1842ac641cb354";
-  public static final String GIT_DATE = "2025-01-14 10:10:58 AWST";
+  public static final int GIT_REVISION = 39;
+  public static final String GIT_SHA = "37ffe3a14d3f36dd064950b38af089db39e14e63";
+  public static final String GIT_DATE = "2025-01-14 10:14:19 AWST";
   public static final String GIT_BRANCH = "jade/auto-align";
-  public static final String BUILD_DATE = "2025-01-14 10:11:50 AWST";
-  public static final long BUILD_UNIX_TIME = 1736820710739L;
-  public static final int DIRTY = 0;
+  public static final String BUILD_DATE = "2025-01-14 10:15:30 AWST";
+  public static final long BUILD_UNIX_TIME = 1736820930079L;
+  public static final int DIRTY = 1;
 
   private BuildConstants() {}
 }
