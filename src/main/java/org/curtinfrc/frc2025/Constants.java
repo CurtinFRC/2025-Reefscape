c26f5cc92f4c721d850fb7fda5033ad5538c57f4
Paul Hodges <paul.hodges@student.education.wa.edu.au>
2025-01-16 13:44:23 +0800

    small adjustments

--- src/main/java/org/curtinfrc/frc2025/Constants.java
+++ src/main/java/org/curtinfrc/frc2025/Constants.java
@@ -50,12 +50,6 @@ public final class Constants {
     return robotType;
   }
 
-  // public static Alliance getAlliance() {
-  //   return getMode() == Mode.SIM ? (List.of(AllianceStationID.Blue1, AllianceStationID.Blue2,
-  // AllianceStationID.Blue3).contains(DriverStationSim.getAllianceStationId()) ? Alliance.Blue :
-  // Alliance.Red) : DriverStation.getAlliance().get();
-  // }
-
   public static enum RobotType {
     /** Running in simulation */
     SIMBOT,
