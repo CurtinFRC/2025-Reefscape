447b8a42c47d14485f36e1710ff80b4370c46847
Jade <spacey-sooty@proton.me>
2025-01-18 21:07:36 +0800

    [build] Update AdvantageKit to 4.1.0 (#20)
    Signed-off-by: Jade Turner <spacey-sooty@proton.me>

--- src/main/java/org/curtinfrc/frc2025/BuildConstants.java
+++ src/main/java/org/curtinfrc/frc2025/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025-Reefscape";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 26;
-  public static final String GIT_SHA = "8094eede2fe3f623a77141baf8fec2f94d7f662b";
-  public static final String GIT_DATE = "2025-01-16 18:03:56 AWST";
-  public static final String GIT_BRANCH = "jade/2025-kickoff";
-  public static final String BUILD_DATE = "2025-01-16 18:12:25 AWST";
-  public static final long BUILD_UNIX_TIME = 1737022345060L;
+  public static final int GIT_REVISION = 25;
+  public static final String GIT_SHA = "72130f7d932d170d3dea94232be7709d480f551b";
+  public static final String GIT_DATE = "2025-01-16 18:27:11 AWST";
+  public static final String GIT_BRANCH = "jade/update-akit";
+  public static final String BUILD_DATE = "2025-01-18 14:50:56 AWST";
+  public static final long BUILD_UNIX_TIME = 1737183056497L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
