0b10ae048494bfc84b04e14e52241ce2efa3584b
Jade Turner <spacey-sooty@proton.me>
2025-01-18 22:05:52 +0800

    Add TrajoptLib
    Signed-off-by: Jade Turner <spacey-sooty@proton.me>

--- src/main/java/org/curtinfrc/frc2025/BuildConstants.java
+++ src/main/java/org/curtinfrc/frc2025/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025-Reefscape";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 32;
-  public static final String GIT_SHA = "228c8b68aa27d11de4c7f2d949068925bc4a304b";
-  public static final String GIT_DATE = "2025-01-19 21:05:48 AWST";
+  public static final int GIT_REVISION = 33;
+  public static final String GIT_SHA = "c02ca801e6b1ef3a366b050ea65a8162bd71a85e";
+  public static final String GIT_DATE = "2025-01-19 21:06:05 AWST";
   public static final String GIT_BRANCH = "HEAD";
-  public static final String BUILD_DATE = "2025-01-19 21:05:55 AWST";
-  public static final long BUILD_UNIX_TIME = 1737291955959L;
+  public static final String BUILD_DATE = "2025-01-19 21:06:18 AWST";
+  public static final long BUILD_UNIX_TIME = 1737291978786L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
