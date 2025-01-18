f84f3f94f5b5a55fdd6f31886fce566951b04bbd
Jade <spacey-sooty@proton.me>
2025-01-18 21:24:59 +0800

    Check robot type when deploying code (#21)
    Signed-off-by: Jade Turner <spacey-sooty@proton.me>

--- src/main/java/org/curtinfrc/frc2025/BuildConstants.java
+++ src/main/java/org/curtinfrc/frc2025/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025-Reefscape";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 25;
-  public static final String GIT_SHA = "72130f7d932d170d3dea94232be7709d480f551b";
-  public static final String GIT_DATE = "2025-01-16 18:27:11 AWST";
-  public static final String GIT_BRANCH = "jade/update-akit";
-  public static final String BUILD_DATE = "2025-01-18 14:50:56 AWST";
-  public static final long BUILD_UNIX_TIME = 1737183056497L;
+  public static final int GIT_REVISION = 27;
+  public static final String GIT_SHA = "caf739fc63777702c2c76e8780b7094f06c0cddd";
+  public static final String GIT_DATE = "2025-01-18 21:18:56 AWST";
+  public static final String GIT_BRANCH = "jade/is-deploy-valid";
+  public static final String BUILD_DATE = "2025-01-18 21:21:52 AWST";
+  public static final long BUILD_UNIX_TIME = 1737206512515L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
