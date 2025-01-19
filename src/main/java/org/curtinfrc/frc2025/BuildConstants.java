887b29a88fa9691c7308ec26a5c066dd012b473f
Jade Turner <spacey-sooty@proton.me>
2025-01-19 09:59:05 +0800

    Fix auto chooser not working
    Signed-off-by: Jade Turner <spacey-sooty@proton.me>

--- src/main/java/org/curtinfrc/frc2025/BuildConstants.java
+++ src/main/java/org/curtinfrc/frc2025/BuildConstants.java
@@ -6,11 +6,11 @@ public final class BuildConstants {
   public static final String MAVEN_NAME = "2025-Reefscape";
   public static final String VERSION = "unspecified";
   public static final int GIT_REVISION = 27;
-  public static final String GIT_SHA = "caf739fc63777702c2c76e8780b7094f06c0cddd";
-  public static final String GIT_DATE = "2025-01-18 21:18:56 AWST";
-  public static final String GIT_BRANCH = "jade/is-deploy-valid";
-  public static final String BUILD_DATE = "2025-01-18 21:21:52 AWST";
-  public static final long BUILD_UNIX_TIME = 1737206512515L;
+  public static final String GIT_SHA = "f84f3f94f5b5a55fdd6f31886fce566951b04bbd";
+  public static final String GIT_DATE = "2025-01-18 21:24:59 AWST";
+  public static final String GIT_BRANCH = "master";
+  public static final String BUILD_DATE = "2025-01-19 09:58:32 AWST";
+  public static final long BUILD_UNIX_TIME = 1737251912933L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
