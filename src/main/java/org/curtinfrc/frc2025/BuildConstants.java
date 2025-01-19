870cbf207af9b7ab79238e0a78a5e80baf3afc2a
Jade <spacey-sooty@proton.me>
2025-01-19 14:01:07 +0800

    [drive] Fix trajectory logging (#30)
    Signed-off-by: Jade Turner <spacey-sooty@proton.me>

--- src/main/java/org/curtinfrc/frc2025/BuildConstants.java
+++ src/main/java/org/curtinfrc/frc2025/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025-Reefscape";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 27;
-  public static final String GIT_SHA = "f84f3f94f5b5a55fdd6f31886fce566951b04bbd";
-  public static final String GIT_DATE = "2025-01-18 21:24:59 AWST";
+  public static final int GIT_REVISION = 29;
+  public static final String GIT_SHA = "107791ee3df453a999372eec53f64f957ce5a0d0";
+  public static final String GIT_DATE = "2025-01-19 10:08:32 AWST";
   public static final String GIT_BRANCH = "master";
-  public static final String BUILD_DATE = "2025-01-19 09:58:32 AWST";
-  public static final long BUILD_UNIX_TIME = 1737251912933L;
+  public static final String BUILD_DATE = "2025-01-19 13:58:34 AWST";
+  public static final long BUILD_UNIX_TIME = 1737266314321L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
