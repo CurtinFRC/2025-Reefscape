61a4be9099e97ad5a07efb7a8f268b27c8e2d993
Jade Turner <spacey-sooty@proton.me>
2025-01-25 01:05:11 +0800

    Fix SparkUtil and actually configure intake and ejector motors
    Signed-off-by: Jade Turner <spacey-sooty@proton.me>

--- src/main/java/org/curtinfrc/frc2025/subsystems/intake/IntakeIONEO.java
+++ src/main/java/org/curtinfrc/frc2025/subsystems/intake/IntakeIONEO.java
@@ -2,11 +2,14 @@ package org.curtinfrc.frc2025.subsystems.intake;
 
 import static org.curtinfrc.frc2025.subsystems.intake.IntakeConstants.*;
 
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 import com.revrobotics.spark.config.SparkMaxConfig;
 import edu.wpi.first.wpilibj.DigitalInput;
+import org.curtinfrc.frc2025.util.SparkUtil;
 
 public class IntakeIONEO implements IntakeIO {
   private final SparkMax intakeNeo = new SparkMax(intakeMotorId, MotorType.kBrushless);
@@ -16,6 +19,11 @@ public class IntakeIONEO implements IntakeIO {
   public IntakeIONEO() {
     SparkMaxConfig config = new SparkMaxConfig();
     config.smartCurrentLimit(0, intakeCurrentLimit).idleMode(IdleMode.kCoast);
+    SparkUtil.tryUntilOk(
+        5,
+        () ->
+            intakeNeo.configure(
+                config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters));
   }
 
   @Override
