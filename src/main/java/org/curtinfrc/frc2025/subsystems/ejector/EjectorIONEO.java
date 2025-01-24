b930ead40cb680503383ebe14bb8629a87a9ff35
Jade <spacey-sooty@proton.me>
2025-01-25 01:10:05 +0800

    Fix SparkUtil and actually configure intake and ejector motors (#44)
    Signed-off-by: Jade Turner <spacey-sooty@proton.me>

--- src/main/java/org/curtinfrc/frc2025/subsystems/ejector/EjectorIONEO.java
+++ src/main/java/org/curtinfrc/frc2025/subsystems/ejector/EjectorIONEO.java
@@ -2,32 +2,40 @@ package org.curtinfrc.frc2025.subsystems.ejector;
 
 import static org.curtinfrc.frc2025.subsystems.ejector.EjectorConstants.*;
 
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 import com.revrobotics.spark.config.SparkMaxConfig;
 import edu.wpi.first.wpilibj.DigitalInput;
+import org.curtinfrc.frc2025.util.SparkUtil;
 
 public class EjectorIONEO implements EjectorIO {
-  private final SparkMax intakeNeo = new SparkMax(motorId, MotorType.kBrushless);
+  private final SparkMax neo = new SparkMax(motorId, MotorType.kBrushless);
   private final DigitalInput sensor = new DigitalInput(sensorPort);
 
   public EjectorIONEO() {
     SparkMaxConfig config = new SparkMaxConfig();
     config.smartCurrentLimit(0, currentLimit).idleMode(IdleMode.kCoast);
+
+    SparkUtil.tryUntilOk(
+        5,
+        () ->
+            neo.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters));
   }
 
   @Override
   public void updateInputs(EjectorIOInputs inputs) {
-    inputs.appliedVolts = intakeNeo.getBusVoltage() * intakeNeo.getAppliedOutput();
-    inputs.currentAmps = intakeNeo.getOutputCurrent();
-    inputs.positionRotations = intakeNeo.getEncoder().getPosition();
-    inputs.angularVelocityRotationsPerMinute = intakeNeo.getEncoder().getVelocity();
+    inputs.appliedVolts = neo.getBusVoltage() * neo.getAppliedOutput();
+    inputs.currentAmps = neo.getOutputCurrent();
+    inputs.positionRotations = neo.getEncoder().getPosition();
+    inputs.angularVelocityRotationsPerMinute = neo.getEncoder().getVelocity();
     inputs.sensor = sensor.get();
   }
 
   @Override
   public void setVoltage(double volts) {
-    intakeNeo.setVoltage(volts);
+    neo.setVoltage(volts);
   }
 }
