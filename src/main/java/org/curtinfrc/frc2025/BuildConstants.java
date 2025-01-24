4c4f1ef10b6e7f9fe83c84d0f61c14c99a4b7edf
Paul Hodges <paul.hodges@student.education.wa.edu.au>
2025-01-24 12:23:14 +0800

    merge

--- src/main/java/org/curtinfrc/frc2025/BuildConstants.java
+++ src/main/java/org/curtinfrc/frc2025/BuildConstants.java
@@ -5,13 +5,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025-Reefscape";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 46;
-  public static final String GIT_SHA = "d84b4423208cba8a340d370cdf9ca54fcb9dcd31";
-  public static final String GIT_DATE = "2025-01-23 17:58:03 AWST";
+  public static final int GIT_REVISION = 45;
+  public static final String GIT_SHA = "9fc4b9afa35ec7f3eaf252c12769966fc21bd42f";
+  public static final String GIT_DATE = "2025-01-24 12:21:39 AWST";
   public static final String GIT_BRANCH = "paul/mpc";
-  public static final String BUILD_DATE = "2025-01-24 10:43:50 AWST";
-  public static final long BUILD_UNIX_TIME = 1737686630605L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2025-01-24 12:22:08 AWST";
+  public static final long BUILD_UNIX_TIME = 1737692528741L;
+  public static final int DIRTY = 0;
 
   private BuildConstants() {}
 }
