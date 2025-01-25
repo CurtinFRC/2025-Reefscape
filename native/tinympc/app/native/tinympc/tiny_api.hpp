b249602d5758c809758ece19f818278ac984850f
Paul Hodges <paul.hodges@student.education.wa.edu.au>
2025-01-25 17:57:48 +0800

    i mean :skull:

--- native/tinympc/app/native/tinympc/tiny_api.hpp
+++ native/tinympc/app/native/tinympc/tiny_api.hpp
@@ -17,25 +17,25 @@
 extern "C" {
 #endif
 
-int tiny_setup(TinySolver** solverp,
+TINY_API int tiny_setup(TinySolver** solverp,
                 tinyMatrix Adyn, tinyMatrix Bdyn, tinyMatrix Q, tinyMatrix R, 
                 tinytype rho, int nx, int nu, int N,
                 tinyMatrix x_min, tinyMatrix x_max, tinyMatrix u_min, tinyMatrix u_max,
                 int verbose);
-int tiny_precompute_and_set_cache(TinyCache *cache, 
+TINY_API int tiny_precompute_and_set_cache(TinyCache *cache, 
                                     tinyMatrix Adyn, tinyMatrix Bdyn, tinyMatrix Q, tinyMatrix R,
                                     int nx, int nu, tinytype rho, int verbose);
-int tiny_solve(TinySolver *solver);
+TINY_API int tiny_solve(TinySolver *solver);
 
-int tiny_update_settings(TinySettings* settings,
+TINY_API int tiny_update_settings(TinySettings* settings,
                             tinytype abs_pri_tol, tinytype abs_dua_tol, 
                             int max_iter, int check_termination, 
                             int en_state_bound, int en_input_bound);
-int tiny_set_default_settings(TinySettings* settings);
+TINY_API int tiny_set_default_settings(TinySettings* settings);
 
-int tiny_set_x0(TinySolver* solver, tinyVector x0);
-int tiny_set_x_ref(TinySolver* solver, tinyMatrix x_ref);
-int tiny_set_u_ref(TinySolver* solver, tinyMatrix u_ref);
+TINY_API int tiny_set_x0(TinySolver* solver, tinyVector x0);
+TINY_API int tiny_set_x_ref(TinySolver* solver, tinyMatrix x_ref);
+TINY_API int tiny_set_u_ref(TinySolver* solver, tinyMatrix u_ref);
 
 
 #ifdef __cplusplus
--- src/main/java/org/curtinfrc/frc2025/BuildConstants.java
+++ src/main/java/org/curtinfrc/frc2025/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025-Reefscape";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 50;
-  public static final String GIT_SHA = "66c642ce9c2b5d3bce424fb11ca39ddf9ac4b4b9";
-  public static final String GIT_DATE = "2025-01-25 17:20:44 AWST";
+  public static final int GIT_REVISION = 51;
+  public static final String GIT_SHA = "0e2e9a7a8123be63ced9533e31717d3b3683c244";
+  public static final String GIT_DATE = "2025-01-25 17:26:48 AWST";
   public static final String GIT_BRANCH = "paul/jni";
-  public static final String BUILD_DATE = "2025-01-25 17:26:20 AWST";
-  public static final long BUILD_UNIX_TIME = 1737797180046L;
+  public static final String BUILD_DATE = "2025-01-25 17:56:37 AWST";
+  public static final long BUILD_UNIX_TIME = 1737798997927L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
