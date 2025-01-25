0e2e9a7a8123be63ced9533e31717d3b3683c244
Paul Hodges <paul.hodges@student.education.wa.edu.au>
2025-01-25 17:26:48 +0800

    maybe again?

--- src/main/java/org/curtinfrc/frc2025/BuildConstants.java
+++ src/main/java/org/curtinfrc/frc2025/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025-Reefscape";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 49;
-  public static final String GIT_SHA = "902c952a63eaa8cac0600f4adbfa4b422738644d";
-  public static final String GIT_DATE = "2025-01-25 17:19:05 AWST";
+  public static final int GIT_REVISION = 50;
+  public static final String GIT_SHA = "66c642ce9c2b5d3bce424fb11ca39ddf9ac4b4b9";
+  public static final String GIT_DATE = "2025-01-25 17:20:44 AWST";
   public static final String GIT_BRANCH = "paul/jni";
-  public static final String BUILD_DATE = "2025-01-25 17:20:30 AWST";
-  public static final long BUILD_UNIX_TIME = 1737796830631L;
+  public static final String BUILD_DATE = "2025-01-25 17:26:20 AWST";
+  public static final long BUILD_UNIX_TIME = 1737797180046L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
