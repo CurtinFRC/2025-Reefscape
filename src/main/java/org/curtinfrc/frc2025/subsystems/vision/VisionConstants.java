8876bc3f554320adf034b977d9b4d242825d4f85
Jade Turner <spacey-sooty@proton.me>
2025-02-01 13:50:16 +0800

    fix offsets
    Signed-off-by: Jade Turner <spacey-sooty@proton.me>

--- src/main/java/org/curtinfrc/frc2025/subsystems/vision/VisionConstants.java
+++ src/main/java/org/curtinfrc/frc2025/subsystems/vision/VisionConstants.java
@@ -33,8 +33,8 @@ public class VisionConstants {
   // (Not used by Limelight, configure in web UI instead)
   public static Transform3d robotToCamera0 =
       new Transform3d(0.01, 0.06, 0.360, new Rotation3d(0, -Math.PI / 6, 0));
-  public static Transform3d robotToCamera1 = new Transform3d(0.215, 0.16, 0.305, Rotation3d.kZero);
-  public static Transform3d robotToCamera2 = new Transform3d(-0.215, 0.16, 0.305, Rotation3d.kZero);
+  public static Transform3d robotToCamera1 = new Transform3d(0.16, -0.215, 0.305, Rotation3d.kZero);
+  public static Transform3d robotToCamera2 = new Transform3d(0.16, 0.215, 0.305, Rotation3d.kZero);
 
   // Basic filtering thresholds
   public static double maxAmbiguity = 0.3;
