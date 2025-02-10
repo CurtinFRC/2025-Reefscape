d02c447da4d0406c6b03a229a118885c83b028e4
Jade Turner <spacey-sooty@proton.me>
2025-02-10 17:35:51 +0800

    Cycles ready for HP trials
    Signed-off-by: Jade Turner <spacey-sooty@proton.me>

--- src/main/java/org/curtinfrc/frc2025/subsystems/elevator/ElevatorConstants.java
+++ src/main/java/org/curtinfrc/frc2025/subsystems/elevator/ElevatorConstants.java
@@ -5,7 +5,7 @@ public class ElevatorConstants {
   public static int motorPort = 31;
 
   // TODO: TUNE PID and ff
-  public static double tolerance = 0.1;
+  public static double tolerance = 1;
   public static double kP = 0.5;
   public static double kD = 0;
   public static double kV = 0;
