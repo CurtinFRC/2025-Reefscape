9d38a4ab5a95137d60341e2c62128d0587713685
Robert <45889425+robpl4788@users.noreply.github.com>
2025-02-21 19:12:54 +0800

    button board ports fixed

--- src/main/java/org/curtinfrc/frc2025/util/ButtonBoard.java
+++ src/main/java/org/curtinfrc/frc2025/util/ButtonBoard.java
@@ -8,11 +8,11 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
 public class ButtonBoard extends CommandGenericHID implements Sendable {
   public enum Button {
     kAB(7),
-    kCD(6),
-    kEF(2),
+    kCD(13),
+    kEF(1),
     kGH(3),
-    kIJ(1),
-    kKL(13),
+    kIJ(2),
+    kKL(6),
     kProcessor(8),
     kPlayerLeft(5),
     kPlayerRight(4);
