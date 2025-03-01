d4f9e5a94bbf52aa6cfecfc109e0cb15f4012753
Ava Edwards <ava.edwards09@proton.me>
2025-03-01 17:19:06 +0800

    removed unnecessary values from ClimberConstants.java

--- src/main/java/org/curtinfrc/frc2025/subsystems/climber/ClimberConstants.java
+++ src/main/java/org/curtinfrc/frc2025/subsystems/climber/ClimberConstants.java
@@ -3,24 +3,13 @@ package org.curtinfrc.frc2025.subsystems.climber;
 public class ClimberConstants {
   public static int grabberMotorPort = 99;
 
-  public static final double grabberTargetVoltage = 4.0;
-
-  public static double grabberTimeout = 5.0;
-
   public static double targetPositionRotations = 0.0625; // 1/16 of a rotation / 22.5 degrees
 
-  public static int currentLimit = 60; // TODO: find actual value
+  public static int currentLimit = 60;
 
-  public static double kP = 1;
+  public static double kP = 1.0;
   public static double kI = 1.0;
   public static double kD = 0.2;
-  public static double kMinOutput = -1.0;
-  public static double kMaxOutput = 1.0;
-
-  public static double grabberMotorkV = 1.0;
-  public static double grabberMaxVelocity = 1.0;
-  public static double grabberMaxAcceleration = 1.0;
-  public static double grabberAllowedError = 0.0;
 
   public static double grabberTolerance = 0.1;
 }
