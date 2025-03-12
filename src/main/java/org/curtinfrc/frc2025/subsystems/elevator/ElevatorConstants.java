2e1953f2b6f96f20ee3599264bf2504e40a30475
Jade Turner <spacey-sooty@proton.me>
2025-03-12 17:35:41 +0800

    [elevator] Switch to Comp IO structure
    Signed-off-by: Jade Turner <spacey-sooty@proton.me>

--- src/main/java/org/curtinfrc/frc2025/subsystems/elevator/ElevatorConstants.java
+++ src/main/java/org/curtinfrc/frc2025/subsystems/elevator/ElevatorConstants.java
@@ -6,8 +6,6 @@ import edu.wpi.first.util.struct.StructSerializable;
 
 public class ElevatorConstants {
   public static int resetPort = 0;
-  public static int leaderPort = 31;
-  public static int followerPort = 32;
   public static final int currentLimit = 50;
 
   // TODO: TUNE PID and ff
@@ -21,19 +19,6 @@ public class ElevatorConstants {
   public static double climbkP = 45;
   public static double climbkI = 0;
   public static double climbkD = 0;
-
-  public static final double pulleyRadiusMeters = 0.03055;
-  public static final double gearing = 8.1818;
-  public static final double maxHeightMeters = 0.8;
-  public static final double carriageToGround = 0.06229734;
-
-  public static double positionRotationsToMetres(double rotations) {
-    return rotations * Math.PI * 2 * pulleyRadiusMeters / gearing;
-  }
-
-  public static double positionMetresToRotations(double metres) {
-    return metres / (Math.PI * 2 * pulleyRadiusMeters) * gearing;
-  }
 
   // TODO
   public static enum ElevatorSetpoints implements StructSerializable {
--- src/main/java/org/curtinfrc/frc2025/subsystems/elevator/ElevatorIOSim.java
+++ src/main/java/org/curtinfrc/frc2025/subsystems/elevator/ElevatorIOSim.java
@@ -2,15 +2,13 @@ package org.curtinfrc.frc2025.subsystems.elevator;
 
 import static org.curtinfrc.frc2025.subsystems.elevator.ElevatorConstants.kA;
 import static org.curtinfrc.frc2025.subsystems.elevator.ElevatorConstants.kV;
-import static org.curtinfrc.frc2025.subsystems.elevator.ElevatorConstants.maxHeightMeters;
-import static org.curtinfrc.frc2025.subsystems.elevator.ElevatorConstants.positionMetresToRotations;
 
 import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.wpilibj.simulation.ElevatorSim;
 
 public class ElevatorIOSim implements ElevatorIO {
   private final ElevatorSim elevatorSim =
-      new ElevatorSim(kV, kA, DCMotor.getNEO(1), 0, maxHeightMeters, true, 0);
+      new ElevatorSim(kV, kA, DCMotor.getNEO(1), 0, 0.6, true, 0);
   private double volts = 0;
 
   @Override
@@ -19,7 +17,7 @@ public class ElevatorIOSim implements ElevatorIO {
     // ignore velocity cause we dont care
     inputs.currentAmps = elevatorSim.getCurrentDrawAmps();
     inputs.appliedVolts = volts;
-    inputs.positionRotations = positionMetresToRotations(elevatorSim.getPositionMeters());
+    inputs.positionRotations = elevatorSim.getPositionMeters();
   }
 
   @Override
