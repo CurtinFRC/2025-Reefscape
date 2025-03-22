a0187129fdb40e28f3a67c7eb5ffca91442c6825
Jade Turner <spacey-sooty@proton.me>
2025-03-22 11:22:22 +0800

    [intake] Switch to Krakens
    Signed-off-by: Jade Turner <spacey-sooty@proton.me>

--- src/main/java/org/curtinfrc/frc2025/subsystems/intake/IntakeIOComp.java
+++ src/main/java/org/curtinfrc/frc2025/subsystems/intake/IntakeIOComp.java
@@ -1,41 +1,64 @@
 package org.curtinfrc.frc2025.subsystems.intake;
 
-import com.revrobotics.spark.SparkBase.PersistMode;
-import com.revrobotics.spark.SparkBase.ResetMode;
-import com.revrobotics.spark.SparkLowLevel.MotorType;
-import com.revrobotics.spark.SparkMax;
-import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
-import com.revrobotics.spark.config.SparkMaxConfig;
+import static org.curtinfrc.frc2025.util.PhoenixUtil.tryUntilOk;
+
+import com.ctre.phoenix6.BaseStatusSignal;
+import com.ctre.phoenix6.StatusSignal;
+import com.ctre.phoenix6.configs.MotorOutputConfigs;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.VoltageOut;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.InvertedValue;
+import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.units.measure.AngularVelocity;
+import edu.wpi.first.units.measure.Current;
+import edu.wpi.first.units.measure.Voltage;
 import edu.wpi.first.wpilibj.DigitalInput;
-import org.curtinfrc.frc2025.util.SparkUtil;
 
 public class IntakeIOComp implements IntakeIO {
-  private final SparkMax intakeNeo = new SparkMax(30, MotorType.kBrushless);
+  private static final int ID = 20;
+
+  private final TalonFX motor = new TalonFX(ID);
+
   private final DigitalInput frontSensor = new DigitalInput(3);
   private final DigitalInput backSensor = new DigitalInput(5);
 
+  private final StatusSignal<Voltage> voltage = motor.getMotorVoltage();
+  private final StatusSignal<Current> current = motor.getStatorCurrent();
+  private final StatusSignal<Angle> position = motor.getPosition();
+  private final StatusSignal<AngularVelocity> velocity = motor.getVelocity();
+
+  private final VoltageOut voltageRequest = new VoltageOut(0).withEnableFOC(true);
+
   public IntakeIOComp() {
-    SparkMaxConfig config = new SparkMaxConfig();
-    config.smartCurrentLimit(0, 35).idleMode(IdleMode.kCoast).inverted(true);
-    SparkUtil.tryUntilOk(
+    tryUntilOk(
         5,
         () ->
-            intakeNeo.configure(
-                config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters));
+            motor
+                .getConfigurator()
+                .apply(
+                    new TalonFXConfiguration()
+                        .withMotorOutput(
+                            new MotorOutputConfigs()
+                                .withInverted(InvertedValue.Clockwise_Positive))));
+    BaseStatusSignal.setUpdateFrequencyForAll(20.0, velocity, voltage, current, position);
+    motor.optimizeBusUtilization();
   }
 
   @Override
   public void updateInputs(IntakeIOInputs inputs) {
-    inputs.appliedVolts = intakeNeo.getBusVoltage() * intakeNeo.getAppliedOutput();
-    inputs.currentAmps = intakeNeo.getOutputCurrent();
-    inputs.positionRotations = intakeNeo.getEncoder().getPosition();
-    inputs.angularVelocityRotationsPerMinute = intakeNeo.getEncoder().getVelocity();
+    BaseStatusSignal.refreshAll(velocity, voltage, current, position);
+    motor.setControl(voltageRequest);
+    inputs.appliedVolts = voltage.getValueAsDouble();
+    inputs.currentAmps = current.getValueAsDouble();
+    inputs.positionRotations = position.getValueAsDouble();
+    inputs.angularVelocityRotationsPerMinute = velocity.getValueAsDouble();
     inputs.frontSensor = frontSensor.get();
     inputs.backSensor = backSensor.get();
   }
 
   @Override
   public void setVoltage(double volts) {
-    intakeNeo.setVoltage(volts);
+    voltageRequest.withOutput(volts);
   }
 }
