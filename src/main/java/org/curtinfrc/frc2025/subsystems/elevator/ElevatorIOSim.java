060bd365052933eccb205b362c917e0b575c3e9c
Jade Turner <spacey-sooty@proton.me>
2025-05-26 19:27:08 +0800

    [elevator] More accurate simulation

--- src/main/java/org/curtinfrc/frc2025/subsystems/elevator/ElevatorIOSim.java
+++ src/main/java/org/curtinfrc/frc2025/subsystems/elevator/ElevatorIOSim.java
@@ -1,14 +1,11 @@
 package org.curtinfrc.frc2025.subsystems.elevator;
 
-import static org.curtinfrc.frc2025.subsystems.elevator.ElevatorConstants.kA;
-import static org.curtinfrc.frc2025.subsystems.elevator.ElevatorConstants.kV;
-
 import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.wpilibj.simulation.ElevatorSim;
 
 public class ElevatorIOSim implements ElevatorIO {
   private final ElevatorSim elevatorSim =
-      new ElevatorSim(kV, kA, DCMotor.getNEO(1), 0, 0.6, true, 0);
+      new ElevatorSim(3.35, 0.02, DCMotor.getKrakenX60Foc(2), 0, 0.6, true, 0.01);
   private double volts = 0;
 
   @Override
