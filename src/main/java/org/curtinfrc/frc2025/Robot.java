7d3b557f06a76913dc7399338206dd801f2dc51b
Jade <spacey-sooty@proton.me>
2025-06-22 15:07:55 +0800

    [drive] Fix inversions (#136)
    Signed-off-by: Jade Turner <spacey-sooty@proton.me>

--- src/main/java/org/curtinfrc/frc2025/Robot.java
+++ src/main/java/org/curtinfrc/frc2025/Robot.java
@@ -174,7 +174,8 @@ public class Robot extends LoggedRobot {
                   drive::addVisionMeasurement,
                   new VisionIOPhotonVision(camera0Name, robotToCamera0),
                   new VisionIOPhotonVision(camera1Name, robotToCamera1),
-                  new VisionIOPhotonVision(camera2Name, robotToCamera2),
+                  // new VisionIOPhotonVision(camera2Name, robotToCamera2),
+                  new VisionIO() {},
                   new VisionIOPhotonVision(camera3Name, robotToCamera3));
           elevator = new Elevator(new ElevatorIOComp());
           intake = new Intake(new IntakeIOComp());
@@ -336,9 +337,9 @@ public class Robot extends LoggedRobot {
     // Default command, normal field-relative drive
     drive.setDefaultCommand(
         drive.joystickDrive(
-            () -> -controller.getLeftY(),
-            () -> -controller.getLeftX(),
-            () -> -controller.getRightX()));
+            () -> controller.getLeftY(),
+            () -> controller.getLeftX(),
+            () -> controller.getRightX()));
 
     drive
         .atSetpoint
