13fd8b12eae0b888f5370e7ad199e519846b8b05
Sean Chan <sean.chan3@student.education.wa.edu.au>
2025-07-19 12:35:45 +0800

    ran ./gradlew build

--- src/main/java/org/curtinfrc/frc2025/subsystems/processor/Processor.java
+++ src/main/java/org/curtinfrc/frc2025/subsystems/processor/Processor.java
@@ -1,33 +1,30 @@
 package org.curtinfrc.frc2025.subsystems.processor;
 
-import java.util.logging.Logger;
-
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import edu.wpi.first.wpilibj2.command.button.Trigger;
 
 public class Processor extends SubsystemBase {
-    private final ProcessorIO io;
-    private final ProcessorIOInputsAutoLogged inputs = new ProcessorIOInputsAutoLogged();
-    
-    public Processor(ProcessorIO io) {
-        this.io = io;
-    }
+  private final ProcessorIO io;
+  private final ProcessorIOInputsAutoLogged inputs = new ProcessorIOInputsAutoLogged();
 
-    // public final Trigger processorSensor = new Trigger(() -> inputs.processorSensor);
+  public Processor(ProcessorIO io) {
+    this.io = io;
+  }
 
-    @Override
-    public void periodic() {}
+  // public final Trigger processorSensor = new Trigger(() -> inputs.processorSensor);
 
-      public Command stop() {
-        return run(() -> io.setVoltage(0));
-      }
+  @Override
+  public void periodic() {}
 
-      public Command intake(double volts) {
-        return run(() -> io.setVoltage(volts));
-      }
+  public Command stop() {
+    return run(() -> io.setVoltage(0));
+  }
 
-      public Command intake() {
-        return run(() -> io.setVoltage(3));
-      }
-    }
+  public Command intake(double volts) {
+    return run(() -> io.setVoltage(volts));
+  }
+
+  public Command intake() {
+    return run(() -> io.setVoltage(3));
+  }
+}
