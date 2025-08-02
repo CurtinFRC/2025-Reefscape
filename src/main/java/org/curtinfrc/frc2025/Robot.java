4d290b38c377d86118cd5a3f4d4b3a29b9f9fb66
Sean Chan <sean.chan3@student.education.wa.edu.au>
2025-08-02 14:44:31 +0800

    \split algae popper into seperate subsystem

--- src/main/java/org/curtinfrc/frc2025/Robot.java
+++ src/main/java/org/curtinfrc/frc2025/Robot.java
@@ -55,6 +55,10 @@ import org.curtinfrc.frc2025.subsystems.intake.IntakeIOSim;
 import org.curtinfrc.frc2025.subsystems.leds.LEDs;
 import org.curtinfrc.frc2025.subsystems.leds.LEDsIO;
 import org.curtinfrc.frc2025.subsystems.leds.LEDsIOComp;
+import org.curtinfrc.frc2025.subsystems.popper.Popper;
+import org.curtinfrc.frc2025.subsystems.popper.PopperIO;
+import org.curtinfrc.frc2025.subsystems.popper.PopperIOComp;
+import org.curtinfrc.frc2025.subsystems.popper.PopperIOSim;
 import org.curtinfrc.frc2025.subsystems.processor.Processor;
 import org.curtinfrc.frc2025.subsystems.processor.ProcessorIO;
 import org.curtinfrc.frc2025.subsystems.processor.ProcessorIOComp;
@@ -92,6 +96,7 @@ public class Robot extends LoggedRobot {
   private LEDs leds;
   private Elevator elevator;
   private Ejector ejector;
+  private Popper popper;
   private Climber climber;
 
   // Controller
@@ -186,6 +191,7 @@ public class Robot extends LoggedRobot {
           intake = new Intake(new IntakeIOComp());
           processor = new Processor(new ProcessorIOComp());
           ejector = new Ejector(new EjectorIOComp());
+          popper = new Popper(new PopperIOComp());
           climber = new Climber(new ClimberIOComp());
           leds = new LEDs(new LEDsIOComp());
         }
@@ -210,6 +216,7 @@ public class Robot extends LoggedRobot {
           intake = new Intake(new IntakeIO() {});
           processor = new Processor(new ProcessorIO() {});
           ejector = new Ejector(new EjectorIO() {});
+          popper = new Popper(new PopperIO() {});
           climber = new Climber(new ClimberIO() {});
           leds = new LEDs(new LEDsIO() {});
         }
@@ -232,8 +239,9 @@ public class Robot extends LoggedRobot {
 
           elevator = new Elevator(new ElevatorIOSim());
           intake = new Intake(new IntakeIOSim());
-          processor = new Processor(new ProcessorIOSim() {});
+          processor = new Processor(new ProcessorIOSim());
           ejector = new Ejector(new EjectorIOSim());
+          popper = new Popper(new PopperIOSim());
           climber = new Climber(new ClimberIOSim());
           leds = new LEDs(new LEDsIO() {});
         }
@@ -259,6 +267,7 @@ public class Robot extends LoggedRobot {
       intake = new Intake(new IntakeIO() {});
       processor = new Processor(new ProcessorIO() {});
       ejector = new Ejector(new EjectorIO() {});
+      popper = new Popper(new PopperIO() {});
       climber = new Climber(new ClimberIO() {});
       leds = new LEDs(new LEDsIO() {});
     }
@@ -472,7 +481,7 @@ public class Robot extends LoggedRobot {
                         () -> -controller.getLeftY(),
                         () -> -controller.getLeftX(),
                         () -> -controller.getRightX()),
-                    ejector.eject(40),
+                    popper.pop(40),
                     elevator.goToSetpoint(
                         () -> {
                           return switch (DriveSetpoints.closest(drive::getPose, leftSetpoints)) {
---
+++ src/main/java/org/curtinfrc/frc2025/subsystems/popper/Popper.java
@@ -0,0 +1,35 @@
+package org.curtinfrc.frc2025.subsystems.popper;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import org.littletonrobotics.junction.Logger;
+
+public class Popper extends SubsystemBase {
+  private final PopperIO io;
+  private final PopperIOInputsAutoLogged inputs = new PopperIOInputsAutoLogged();
+
+  public Popper(PopperIO io) {
+    this.io = io;
+  }
+
+  @Override
+  public void periodic() {
+    io.updateInputs(inputs);
+    Logger.processInputs("Popper", inputs);
+  }
+
+  public Command stop() {
+    return runOnce(() -> io.setVoltage(0));
+  }
+
+  public Command pop(double rotationsPerSecond) {
+    return run(() -> {
+          io.setVelocity(rotationsPerSecond);
+        })
+        .withName("Pop");
+  }
+
+  public Command setVoltage(double volts) {
+    return run(() -> io.setVoltage(volts));
+  }
+}
