957ac813791e934c5fed3ef59a64bbeddc8016ac
Jade Turner <spacey-sooty@proton.me>
2025-08-10 21:17:43 +0800

    [ejector] Detect when algae has been popped
    Signed-off-by: Jade Turner <spacey-sooty@proton.me>

--- src/main/java/org/curtinfrc/frc2025/subsystems/ejector/Ejector.java
+++ src/main/java/org/curtinfrc/frc2025/subsystems/ejector/Ejector.java
@@ -3,6 +3,7 @@ package org.curtinfrc.frc2025.subsystems.ejector;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
+import org.littletonrobotics.junction.AutoLogOutput;
 import org.littletonrobotics.junction.Logger;
 
 public class Ejector extends SubsystemBase {
@@ -12,6 +13,37 @@ public class Ejector extends SubsystemBase {
   public Ejector(EjectorIO io) {
     this.io = io;
   }
+
+  @AutoLogOutput(key = "Ejector/HasTried")
+  private boolean hasTried = false;
+
+  @AutoLogOutput(key = "Ejector/HasContacted")
+  private boolean hasContacted = false;
+
+  @AutoLogOutput(key = "Ejector/HasPopped")
+  public final Trigger hasPopped =
+      new Trigger(
+          () -> {
+            if (hasContacted) {
+              hasTried = false;
+              var ret = inputs.angularVelocityRotationsPerSecond > 50 && inputs.currentAmps < 45;
+              if (ret) {
+                hasContacted = false;
+              }
+              return ret;
+            }
+
+            if (hasTried) {
+              hasContacted =
+                  inputs.angularVelocityRotationsPerSecond < 45 && inputs.currentAmps > 45;
+            }
+
+            if (!hasTried && !hasContacted) {
+              hasTried = inputs.angularVelocityRotationsPerSecond > 50 && inputs.currentAmps > 2;
+            }
+
+            return false;
+          });
 
   public final Trigger backSensor = new Trigger(() -> inputs.backSensor);
   public final Trigger frontSensor = new Trigger(() -> inputs.frontSensor);
