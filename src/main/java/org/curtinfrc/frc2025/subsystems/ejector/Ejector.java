f20e8df115cbc1a2ee8158a8c60436cb44422dd5
Jade Turner <spacey-sooty@proton.me>
2025-08-27 14:02:19 +0800

    Merge popper and ejector again
    Signed-off-by: Jade Turner <spacey-sooty@proton.me>

--- src/main/java/org/curtinfrc/frc2025/subsystems/ejector/Ejector.java
+++ src/main/java/org/curtinfrc/frc2025/subsystems/ejector/Ejector.java
@@ -3,6 +3,7 @@ package org.curtinfrc.frc2025.subsystems.ejector;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
+import org.littletonrobotics.junction.AutoLogOutput;
 import org.littletonrobotics.junction.Logger;
 
 public class Ejector extends SubsystemBase {
@@ -12,6 +13,33 @@ public class Ejector extends SubsystemBase {
   public Ejector(EjectorIO io) {
     this.io = io;
   }
+
+  @AutoLogOutput(key = "Ejector/HasTried")
+  private boolean hasTried = false;
+
+  @AutoLogOutput(key = "Ejector/HasContacted")
+  private boolean hasContacted = false;
+
+  @AutoLogOutput(key = "Ejector/HasPopped")
+  public final Trigger hasPopped =
+      new Trigger(
+          () -> {
+            if (hasContacted) {
+              hasTried = false;
+              var ret = inputs.angularVelocityRotationsPerSecond > 35 && inputs.currentAmps < 45;
+              if (ret) {
+                hasContacted = false;
+              }
+              return ret;
+            }
+
+            if (hasTried) {
+              hasContacted =
+                  inputs.angularVelocityRotationsPerSecond < 35 && inputs.currentAmps > 50;
+            }
+
+            return false;
+          });
 
   public final Trigger backSensor = new Trigger(() -> inputs.backSensor);
   public final Trigger frontSensor = new Trigger(() -> inputs.frontSensor);
@@ -23,17 +51,32 @@ public class Ejector extends SubsystemBase {
   }
 
   public Command stop() {
-    return runOnce(() -> io.setVoltage(0));
+    return runOnce(() -> io.setVoltage(0)).withName("Stop");
   }
 
-  public Command eject(double rotationsPerSecond) {
+  public Command eject(double speed) {
     return run(() -> {
-          io.setVelocity(rotationsPerSecond);
+          io.setVelocity(speed);
         })
-        .withName("Eject");
+        .withName("Eject " + speed);
+  }
+
+  public Command pop() {
+    return runOnce(() -> hasTried = true)
+        .andThen(
+            run(
+                () -> {
+                  io.setVelocity(30);
+                }))
+        .finallyDo(
+            () -> {
+              hasTried = false;
+              hasContacted = false;
+            })
+        .withName("Pop");
   }
 
   public Command setVoltage(double volts) {
-    return run(() -> io.setVoltage(volts));
+    return run(() -> io.setVoltage(volts)).withName("Run Voltage " + volts);
   }
 }
--- src/main/java/org/curtinfrc/frc2025/subsystems/ejector/EjectorIOComp.java
+++ src/main/java/org/curtinfrc/frc2025/subsystems/ejector/EjectorIOComp.java
@@ -8,6 +8,7 @@ import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
 import com.ctre.phoenix6.configs.MotorOutputConfigs;
 import com.ctre.phoenix6.configs.Slot0Configs;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.Follower;
 import com.ctre.phoenix6.controls.VelocityVoltage;
 import com.ctre.phoenix6.controls.VoltageOut;
 import com.ctre.phoenix6.hardware.TalonFX;
@@ -20,19 +21,25 @@ import edu.wpi.first.units.measure.Voltage;
 import edu.wpi.first.wpilibj.DigitalInput;
 
 public class EjectorIOComp implements EjectorIO {
-  private static final int ID = 46;
+  private static final int ID = 53;
+  private static final int FOLLOWER_ID = 46;
   private static final CurrentLimitsConfigs currentLimits =
-      new CurrentLimitsConfigs().withSupplyCurrentLimit(30).withStatorCurrentLimit(100);
+      new CurrentLimitsConfigs().withSupplyCurrentLimit(30).withStatorCurrentLimit(60);
   private static final TalonFXConfiguration leaderConfig =
       new TalonFXConfiguration()
           .withMotorOutput(
               new MotorOutputConfigs()
-                  .withInverted(InvertedValue.Clockwise_Positive)
+                  .withInverted(InvertedValue.CounterClockwise_Positive)
                   .withNeutralMode(NeutralModeValue.Brake))
-          .withSlot0(new Slot0Configs().withKP(1).withKV(0.33))
+          .withSlot0(new Slot0Configs().withKP(0.5).withKV(0.33))
           .withCurrentLimits(currentLimits);
+  private static final TalonFXConfiguration followerConfig =
+      new TalonFXConfiguration()
+          .withCurrentLimits(currentLimits)
+          .withMotorOutput(new MotorOutputConfigs().withInverted(InvertedValue.Clockwise_Positive));
 
   private final TalonFX motor = new TalonFX(ID);
+  private final TalonFX follower = new TalonFX(FOLLOWER_ID);
 
   private final DigitalInput frontSensor = new DigitalInput(4);
   private final DigitalInput backSensor = new DigitalInput(2);
@@ -44,9 +51,11 @@ public class EjectorIOComp implements EjectorIO {
 
   private final VoltageOut voltageRequest = new VoltageOut(0).withEnableFOC(true);
   private final VelocityVoltage velocityRequest = new VelocityVoltage(0).withEnableFOC(true);
+  private final Follower followRequest = new Follower(ID, true);
 
   public EjectorIOComp() {
     tryUntilOk(5, () -> motor.getConfigurator().apply(leaderConfig));
+    tryUntilOk(5, () -> follower.getConfigurator().apply(followerConfig));
     BaseStatusSignal.setUpdateFrequencyForAll(20.0, velocity, voltage, current, position);
     motor.optimizeBusUtilization();
   }
@@ -54,6 +63,7 @@ public class EjectorIOComp implements EjectorIO {
   @Override
   public void updateInputs(EjectorIOInputs inputs) {
     BaseStatusSignal.refreshAll(velocity, voltage, current, position);
+    follower.setControl(followRequest);
     inputs.appliedVolts = voltage.getValueAsDouble();
     inputs.currentAmps = current.getValueAsDouble();
     inputs.positionRotations = position.getValueAsDouble();
